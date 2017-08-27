edd71dc30233b28af873384cb6f9440ddea760ad
Zarko Jevtic <zarko.jevtic@appmodule.net>
2017-08-27 14:07:44 +0200

    started with statistic

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -17,9 +17,7 @@ import { UserComponent } from './user/user.component';
 import { TablesComponent } from './tables/tables.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import { QueuesComponent } from "./queues/queues.component";
 
 @NgModule({
@@ -30,9 +28,7 @@ import { QueuesComponent } from "./queues/queues.component";
     TablesComponent,
     TypographyComponent,
     IconsComponent,
-    MapsComponent,
     NotificationsComponent,
-    UpgradeComponent,
     QueuesComponent
 
   ],
